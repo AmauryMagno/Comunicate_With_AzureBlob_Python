0debc837160a3a7488f8fd09c0790bda920098fc
Magnoamaury <144865205+AmauryMagno@users.noreply.github.com>
2025-04-15 18:46:45 -0300

    Add Create and list to communicate frontend with database

--- code/main.py
+++ code/main.py
@@ -11,6 +11,7 @@ load_dotenv(dotenv_path=".env")
 # Connect to blob storage
 blobConnectionString = os.getenv("BLOB_CONNECTION_STRING")
 blobContainerName = os.getenv("BLOB_CONTAINER_NAME")
+blobContainerSAS = os.getenv("BLOB_CONTAINER_SAS")
 blobAccountName = os.getenv("BLOB_ACCOUNT_NAME")
 
 # Connect to SQL database
@@ -35,19 +36,24 @@ def upload_blob(file):
     blob_name = str(uuid.uuid4()) + file.name
     blob_client = container_client.get_blob_client(blob_name)
     blob_client.upload_blob(file.read(), overwrite=True)
-    image_url = f"http://{blobAccountName}.blob.core.windows.net/{blobContainerName}/{blob_name}"
+    image_url = f"https://{blobAccountName}.blob.core.windows.net/{blobContainerName}/{blob_name}"
     return image_url
+
+
+def connect_databse():
+    conn = pymssql.connect(
+        server=SQL_SERVER,
+        user=SQL_USER,
+        password=SQL_PASSWORD,
+        database=SQL_DATABASE,
+    )
+    return conn
 
 
 def insert_product(product_name, product_price, product_description, product_image):
     try:
         image_url = upload_blob(product_image)
-        conn = pymssql.connect(
-            server=SQL_SERVER,
-            user=SQL_USER,
-            password=SQL_PASSWORD,
-            database=SQL_DATABASE,
-        )
+        conn = connect_databse()
         cursor = conn.cursor()
         cursor.execute(
             f"INSERT INTO Products (name, price, description, image_url) VALUES('{product_name}', '{product_price}', '{product_description}', '{image_url}')"
@@ -60,11 +66,53 @@ def insert_product(product_name, product_price, product_description, product_ima
         return False
 
 
+def list_products():
+    try:
+        conn = connect_databse()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Products")
+        rows = cursor.fetchall()
+        conn.close()
+        return rows
+    except Exception as e:
+        st.error(f"Erro ao listar produtos: {e}")
+        return []
+
+
+def list_product_show():
+    products = list_products()
+    print(products)
+    if products:
+        # Define number of cadrs per row
+        card_per_row = 3
+        # Creat firts columns
+        cols = st.columns(card_per_row)
+        for i, product in enumerate(products):
+            col = cols[i % card_per_row]
+            with col:
+                st.markdown(f"### {product[1]}")
+                st.write(f"**Descrição:** {product[2]}")
+                st.write(f"**Preço** R$ {product[3]: .2f}")
+                if product[4]:
+                    html_img = f'<img src="{product[4]}?{blobContainerSAS}" width="200" height="200"/>'
+                    print(f"Imagem com SAS: {html_img}")
+                    st.markdown(html_img, unsafe_allow_html=True)
+                st.markdown("----")
+
+            if (i + 1) % card_per_row == 0 and (i + 1) < len(products):
+                cols = st.columns(card_per_row)
+    else:
+        st.info("Nenhum produto encontrado")
+
+
 if st.button("Salvar Produto"):
     insert_product(product_name, product_price, product_description, product_image)
     return_message = "Produto Salvo com Sucesso"
+    list_product_show()
+
 
 st.header("Produtos Cadastrados")
 
 if st.button("Listar Produtos"):
+    list_product_show()
     return_message = "Produtos listados com sucesso"
